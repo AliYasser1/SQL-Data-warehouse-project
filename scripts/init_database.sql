1c1c5bb7e910c0a91866a6321f1a02e7002fda82
Ali Yasser <121754948+AliYasser1@users.noreply.github.com>
2025-08-22 02:24:38 +0300

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,14 @@
+USE master;
 
+--create the database
+CREATE DATABASE DataWarehouse;
+
+USE DataWarehouse;
+
+--create all schemas (bronze, silver, gold)
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
+GO
